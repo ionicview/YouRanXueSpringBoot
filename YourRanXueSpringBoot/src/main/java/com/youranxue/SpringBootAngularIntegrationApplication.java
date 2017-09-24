e7664bc55cd52bb26df7d3f9e8d8f582d479e0ef
ionicview <ionicview@163.com>
2017-09-24 10:47:18 +0900

    YouRanxue SpringBoot

---
+++ YourRanXueSpringBoot/src/main/java/com/youranxue/SpringBootAngularIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.youranxue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularIntegrationApplication.class, args);
+	}
+}
